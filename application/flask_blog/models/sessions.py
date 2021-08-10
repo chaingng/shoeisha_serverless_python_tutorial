c6080953136b939b6af48d4c8ac1a74a8377f7b5
Takatomo Honda <chngng0103@gmail.com>
2021-08-10 18:57:28 +0900

    fix typo

--- application/flask_blog/models/sessions.py
+++ application/flask_blog/models/sessions.py
@@ -8,7 +8,7 @@ class Session(Model):
     class Meta:
         table_name = "serverless_blog_sessions"
         region = app.config.get('DYNAMODB_REGION')
-        aws_access_key_id = app.config.get('AWS_ACEESS_KEY_ID')
+        aws_access_key_id = app.config.get('AWS_ACCESS_KEY_ID')
         aws_secret_access_key = app.config.get('AWS_SECRET_ACCESS_KEY')
         host = app.config.get('DYNAMODB_ENDPOINT_URL')
     SessionId = UnicodeAttribute(hash_key=True, null=False)
--- snippets/list7.2.txt
+++ snippets/list7.2.txt
@@ -7,7 +7,7 @@ class Entry(Model):
     class Meta:
         table_name = "serverless_blog_entries"
         region = app.config.get('DYNAMODB_REGION')
-        aws_access_key_id = app.config.get('AWS_ACEESS_KEY_ID')
+        aws_access_key_id = app.config.get('AWS_ACCESS_KEY_ID')
         aws_secret_access_key = app.config.get('AWS_SECRET_ACCESS_KEY')
         host = app.config.get('DYNAMODB_ENDPOINT_URL')
     id = NumberAttribute(hash_key=True, null=False)
